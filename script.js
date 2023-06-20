c5876f49b0837fd98f12309b11bd9a02e5f4afac
Halip26 <halipuddin.angko@gmail.com>
2023-06-20 15:43:33 +0800

    update the C & = btn

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+// Fungsi Solve yang menerima satu argumen val
 function Solve(val) {
-  var v = document.getElementById("res");
+  // Ambil elemen HTML input dengan ID "res"
+  let v = document.getElementById("res");
+  // Tambahkan nilai argumen val ke nilai elemen input
   v.value += val;
 }
+
+// Fungsi Result yang tidak menerima argumen apapun
 function Result() {
-  var num1 = document.getElementById("res").value;
-  var num2 = eval(num1);
+  // Ambil nilai elemen input dengan ID "res"
+  let num1 = document.getElementById("res").value;
+  // Evaluasi nilai elemen input sebagai ekspresi JavaScript menggunakan fungsi eval()
+  let num2 = eval(num1);
+  // Atur nilai elemen input menjadi hasil evaluasi
   document.getElementById("res").value = num2;
 }
+
+// Fungsi Clear yang tidak menerima argumen apapun
 function Clear() {
-  var inp = document.getElementById("res");
+  // Ambil elemen HTML input dengan ID "res"
+  let inp = document.getElementById("res");
+  // Bersihkan nilai elemen input
   inp.value = "";
 }
+
+// Fungsi Back yang tidak menerima argumen apapun
 function Back() {
-  var ev = document.getElementById("res");
+  // Ambil elemen HTML input dengan ID "res"
+  let ev = document.getElementById("res");
+  // Hapus karakter terakhir dari nilai elemen input
   ev.value = ev.value.slice(0, -1);
 }
