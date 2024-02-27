b8b7c37cb8508c77324bddaad9960621e2b788ce
Halip26 <halipuddin.angko@gmail.com>
2024-02-27 18:50:48 +0800

    update the value id

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // Fungsi Solve yang menerima satu argumen val
 function Solve(val) {
-  // Ambil elemen HTML input dengan ID "res"
-  let v = document.getElementById("res");
+  // Ambil elemen HTML input dengan ID "inputResult"
+  let v = document.getElementById("inputResult");
   // Tambahkan nilai argumen val ke nilai elemen input
   v.value += val;
 }
 
 // Fungsi Result yang tidak menerima argumen apapun
 function Result() {
-  // Ambil nilai elemen input dengan ID "res"
-  let num1 = document.getElementById("res").value;
+  // Ambil nilai elemen input dengan ID "inputResult"
+  let num1 = document.getElementById("inputResult").value;
   // Evaluasi nilai elemen input sebagai ekspresi JavaScript menggunakan fungsi eval()
   let num2 = eval(num1);
   // Atur nilai elemen input menjadi hasil evaluasi
-  document.getElementById("res").value = num2;
+  document.getElementById("inputResult").value = num2;
 }
 
 // Fungsi Clear yang tidak menerima argumen apapun
 function Clear() {
-  // Ambil elemen HTML input dengan ID "res"
-  let inp = document.getElementById("res");
+  // Ambil elemen HTML input dengan ID "inputResult"
+  let inp = document.getElementById("inputResult");
   // Bersihkan nilai elemen input
   inp.value = "";
 }
 
 // Fungsi Back yang tidak menerima argumen apapun
 function Back() {
-  // Ambil elemen HTML input dengan ID "res"
-  let ev = document.getElementById("res");
+  // Ambil elemen HTML input dengan ID "inputResult"
+  let ev = document.getElementById("inputResult");
   // Hapus karakter terakhir dari nilai elemen input
   ev.value = ev.value.slice(0, -1);
 }
